2bcd55aead0532494272c094b09e854374ede811
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-03 23:26:19 -0800

    Edited

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,24 @@
 import React,  { useState } from "react";
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
-import Footer from './Footer';
-import NavBar from './NavBar';
-import './styles/Main.css'
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import "./styles/Main.css"
 
 
 export default function Main() {
-    const [currentPage, setCurrentPage] = useState('Home');
+    const [currentPage, setCurrentPage] = useState("About Me");
   
-    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-    const renderPage = () => {
-      if (currentPage === 'About Me') {
+       const renderPage = () => {
+      if (currentPage === "About Me") {
         return <AboutMe />;
       }
-      if (currentPage === 'Portfolio') {
+      if (currentPage === "Portfolio") {
         return <Portfolio />;
       }
-      if (currentPage === 'Resume') {
+      if (currentPage === "Resume") {
         return <Resume />;
       }
       return <Contact />;
