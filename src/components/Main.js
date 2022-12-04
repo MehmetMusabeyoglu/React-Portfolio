c5002629bafb062573ede30a80dfae7bc3e16d8d
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-03 18:39:07 -0800

    Created initial files and edited app.js

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React,  { useState } from "react";
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Footer from './Footer';
+
+
+
+export default function Main() {
+}
