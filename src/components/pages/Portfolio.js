8db42edc74c31925ed77486706735940350d7339
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-05 19:21:20 -0800

    Edited portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,62 +1,158 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import TextEditor from "../assets/TextEditor.jpg";
+import BookReco from "../assets/BookReco's.png";
+import BILL$ from "../assets/BILL$.jpg";
+import WeatherDashboard from "../assets/WeatherDashboard.jpg";
+import NoteTaker from "../assets/NoteTaker.gif";
+import CodingQuiz from "../assets/CodingQuiz.png";
+
 
 function Portfolio() {
 
     const projects = [
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "Text Editor",
+            "deployed_url": "https://text-editor-2022-mm.herokuapp.com/",
+            "github_url": "https://github.com/MehmetMusabeyoglu/Text-Editor"
         },
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "Book Reco's",
+            "deployed_url": "https://sbhikshe.github.io/Book-Recos/",
+            "github_url": "https://github.com/sbhikshe/Book-Recos"
         },
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "BILL$",
+            "deployed_url": "https://bills-manager2022.herokuapp.com/",
+            "github_url": "https://github.com/danialmirza99/bills-budget-app"
         },
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "Weather Dashboard",
+            "deployed_url": "https://mehmetmusabeyoglu.github.io/Weather-Dashboard/",
+            "github_url": "https://github.com/MehmetMusabeyoglu/Weather-Dashboard"
         },
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "Note Taker",
+            "deployed_url": "https://mehmet-note-taker.herokuapp.com/",
+            "github_url": "https://github.com/MehmetMusabeyoglu/Note-Taker"
         },
         {
-            "title": "",
-            "img_url": "",
-            "deployed_url": "",
-            "github_url": ""
+            "title": "Coding Quiz",
+            "deployed_url": "https://mehmetmusabeyoglu.github.io/Coding-Quiz-Game/",
+            "github_url": "https://github.com/MehmetMusabeyoglu/Coding-Quiz-Game"
         }
     ]
 
     return (
         <div>
-            <h2 className="text-center text-light pt-3 "><b>My Projects</b></h2>
+            <h2 className="text-center text-light pt-3"><b>My Projects</b></h2>
             <div className="container text-center">
-                <Card className="bg-dark text-white">
-                    <Card.Img src={projects.img_url} alt="Card image" />
+                <Card className="text-secondary">
+                    <Card.Img src={TextEditor} alt="Card image" />
                     <Card.ImgOverlay>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
+                        <Card.Title className="text-center text-light pt-5">Text Editor</Card.Title>
+                        <Card.Text className="text-center text-light pt-5">
                             This is a wider card with supporting text below as a natural lead-in
                             to additional content. This content is a little bit longer. <FaGithub />
                         </Card.Text>
                     </Card.ImgOverlay>
                 </Card>
+                <CardGroup>
+                    <Card bg="secondary" >
+                        <Card.Img src={TextEditor} alt="Card image" />
+                        <Card.Body>
+                            <Card.Title className="text-dark"><b> <a className="text-dark" href={projects[0].deployed_url}>Text Editor</a></b> </Card.Title>
+                            <Card.Text className="text-dark">
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                                <a
+                                    className="btn justify-content-md-center mx-1"
+                                    href={projects[0].github_url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    role="button"
+                                    alt="GitHub icon"
+                                >
+                                    <FaGithub color="black" size={25} />
+                                </a>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+                <CardGroup>
+                    <Card>
+                        <Card.Img src={TextEditor} alt="Card image" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+                <CardGroup>
+                    <Card>
+                        <Card.Img src={TextEditor} alt="Card image" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                <b><em>This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</em></b>
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
 
             </div>
         </div>
